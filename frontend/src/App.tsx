f00bfb5b18d29a030c98ffa6c2e4986c0a870ac6
KirSpaceB <productivekirk@gmail.com>
2023-05-03 10:29:10 -0700

    feat: Map is now rendered using arcGis library

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { MapDevelopment } from "./MapDevelopment"
+function App() {
+
+  return (
+    <>
+      <div>
+        <div>Test</div>
+        <MapDevelopment/>
+      </div>
+    </>
+  )
+}
+
+export default App
